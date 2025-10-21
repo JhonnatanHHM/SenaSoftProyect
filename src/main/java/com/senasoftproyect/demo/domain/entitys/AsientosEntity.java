43275ab8e6ab43eccf9f2ddfabf37007767f2ad8
Jhonnatan H. Henao M. <hhmjhonatan@gmail.com>
2025-10-21 18:52:00 -0500

    Back

---
+++ src/main/java/com/senasoftproyect/demo/domain/entitys/AsientosEntity.java
@@ -0,0 +1,10 @@
+package com.senasoftproyect.demo.domain.entitys;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "imagenes")
+public class AsientosEntity {
+
+}
